56a99d7c9655b01b4e22d72e7f8e8a8cc72f0f99
godar <info@tropotek.com>
2018-07-11 09:18:25 +1000

    Minor Code Updates - dev

--- config.php
+++ config.php
@@ -15,15 +15,15 @@ if ($composer)
 
 // Default Home catchall
 $params = array();
-$routes->add('free-presentations', new \Tk\Routing\Route('/events', 'Tk\Ev\Controller\FreePresentations::doDefault', array()));
+$routes->add('events-listing', new \Tk\Routing\Route('/events', 'Tk\Ev\Controller\Listing::doDefault', array()));
 
 $params = array('role' => 'admin');
-$routes->add('event-settings', new \Tk\Routing\Route('/eventSettings.html', 'Tk\Ev\Controller\SystemSettings::doDefault', $params));
+$routes->add('events-settings', new \Tk\Routing\Route('/eventSettings.html', 'Tk\Ev\Controller\SystemSettings::doDefault', $params));
 
-$routes->add('admin-event-manager', new \Tk\Routing\Route('/admin/eventManager.html', 'Tk\Ev\Controller\MailLog\Manager::doDefault', $params));
-$routes->add('admin-event-view', new \Tk\Routing\Route('/admin/eventView.html', 'Tk\Ev\Controller\MailLog\View::doDefault', $params));
+$routes->add('admin-events-manager', new \Tk\Routing\Route('/admin/eventManager.html', 'Tk\Ev\Controller\MailLog\Manager::doDefault', $params));
+$routes->add('admin-events-view', new \Tk\Routing\Route('/admin/eventView.html', 'Tk\Ev\Controller\MailLog\View::doDefault', $params));
 
-$routes->add('admin-event-view', new \Tk\Routing\Route('/admin/eventView.html', 'Tk\Ev\Controller\MailLog\View::doDefault', $params));
+$routes->add('admin-events-view', new \Tk\Routing\Route('/admin/eventView.html', 'Tk\Ev\Controller\MailLog\View::doDefault', $params));
 
 
 
---
+++ sql/mysql/00000.sql
@@ -0,0 +1,37 @@
+-- ----------------------------------------------------
+--
+-- Author: Michael Mifsud <info@tropotek.com>
+--
+-- ----------------------------------------------------
+
+
+CREATE TABLE IF NOT EXISTS `event` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `title` varchar(128) NOT NULL DEFAULT '',
+  `date_start` DATETIME NOT NULL,
+  `date_end` DATETIME NOT NULL,
+  `rsvp` TEXT,
+  `description` TEXT,
+
+  `street` VARCHAR(128) NOT NULL DEFAULT '',
+  `city` VARCHAR(128) NOT NULL DEFAULT '',
+  `state` VARCHAR(128) NOT NULL DEFAULT '',
+  `postcode` VARCHAR(16) NOT NULL DEFAULT '',
+  `country` VARCHAR(128) NOT NULL DEFAULT '',
+  `address` TEXT,                                        -- The full google address: [no] [street], [city], [state], [country]
+  `map_lat` DECIMAL(15,12) NOT NULL DEFAULT 0,
+  `map_lng` DECIMAL(15,12) NOT NULL DEFAULT 0,
+  `map_zoom` DECIMAL(4, 2) NOT NULL DEFAULT 14,
+
+  `notes` TEXT,
+  `active` TINYINT(1) NOT NULL DEFAULT 1,
+  `del` TINYINT(1) NOT NULL DEFAULT 0,
+  `modified` DATETIME NOT NULL,
+  `created` DATETIME NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB;
+
+
+
+
+
